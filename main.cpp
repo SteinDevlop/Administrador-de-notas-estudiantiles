104d656a350ce9f435dbc8fe485a34821602453d
DevStein <83787781+SteinDevlop@users.noreply.github.com>
2024-04-20 16:09:27 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/*
+Administrador de notas estudiantiles (nombre provisional)
+Alejandro Pedro Steinman Cuesta - T00077151
+Andres Rubiano Marrugo - T000
+*/
+#include <iostream>
+using namespace std;
+int main(){
+  return 0;
+}
