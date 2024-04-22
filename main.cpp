123420a1ed425f15336b59e8759df6706740da4c
ZEROANDRES <163555441+ZEROANDRES@users.noreply.github.com>
2024-04-21 19:49:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,22 @@ Alejandro Pedro Steinman Cuesta - T00077151
 Andres Rubiano Marrugo - T000
 */
 #include <iostream>
+#include <string>
+#include <cstdlib>
 using namespace std;
+//prototipos, clases, structs y demás ...
+struct estudiante{ //definición de struct "estudiante"
+  int codigo;
+  string nombre;
+  string carrera;
+  string materia;
+//notadelandres: Yo sugiero hacer un arreglo donde estén las tres notas y la nota final, es decir, float notas [4], se ahorra espacialidad y optimiza mejor;
+  float nota1;
+  float nota2;
+  float nota3;
+  float nota_final;
+};
+// Main
 int main(){
   return 0;
 }
