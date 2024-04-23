536af114bda62abff1eb6e759322844de0623816
DevStein <83787781+SteinDevlop@users.noreply.github.com>
2024-04-23 21:19:54 +0000

    0.1 - Cambios de importe e exporte

--- main.cpp
+++ main.cpp
@@ -9,82 +9,8 @@ Andres Rubiano Marrugo - T000
 #include <fstream>
 #include <iomanip>
 using namespace std;
-//prototipos, clases, structs y demás ...
-struct Estudiante{ //definición de struct "estudiante"
-  int codigo;
-  string nombre;
-  string carrera;
-  float nota1;
-  float nota2;
-  float nota3;
-  float nota_final;
-};
-class comandos{
-  public:
-  void importar(Estudiante p[]){
-    string linea;
-    int n=0; //contar las líneas
-    ifstream leerArchivo; //variable lógica de entrada
-    leerArchivo.open("database.txt");
-    while(!leerArchivo.eof()){
-    getline(leerArchivo,linea);
-    if(linea.size()!=0){//Determina que el archivo ha terminado de leerse
-    n++;}}
-    leerArchivo.close(); //cierre del flujo
-    leerArchivo.open("database.txt");
-    for (int i=0; i<n; i++){
-    getline(leerArchivo,linea,'|');
-    p[i].codigo=atoi(linea.c_str());
-    getline(leerArchivo,linea,'|');
-    p[i].nombre=linea;
-    getline(leerArchivo,linea,'|');
-    p[i].carrera=linea;
-    getline(leerArchivo,linea,'|');
-    p[i].nota1=atof(linea.c_str());
-    getline(leerArchivo,linea,'|');
-    p[i].nota2=atof(linea.c_str());
-    getline(leerArchivo,linea,'|');
-    p[i].nota3=atof(linea.c_str());
-    getline(leerArchivo,linea);
-    p[i].nota_final=atof(linea.c_str());
-  }leerArchivo.close();}
-  void exportar(Estudiante p[], int n){
-    ofstream escribirArchivo;
-    escribirArchivo.open("database.txt");
-    for (int i=0; i<n; i++){
-      escribirArchivo<<p[i].codigo<<"|"<<p[i].nombre<<"|"<<p[i].carrera<<"|"<<p[i].nota1<<"|"<<p[i].nota2<<"|"<<p[i].nota3<<"|"<<p[i].nota_final<<endl;
-    }escribirArchivo.close();}
-  };
-class Data_base: public comandos{
-  private:
-    int tam;
-    Estudiante *database;
-  public:
-    Data_base(int cant): tam(cant){
-      database = new Estudiante [tam];
-    }
-    ~Data_base(){
-      delete[] database;
-    }};
-
-int main(){
-  comandos cls;
-  return 0;
-}
-/*
-/*
-Administrador de notas estudiantiles (nombre provisional)
-Alejandro Pedro Steinman Cuesta - T00077151
-Andres Rubiano Marrugo - T000
-*/
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <fstream>
-#include <iomanip>
-using namespace std;
 //prototipos, clases, structs y demÃ¡s ...
-struct Estudiante{ //definiciÃ³n de struct "estudiante"
+struct Estudiante{
   int codigo;
   string nombre;
   string carrera;
@@ -106,7 +32,7 @@ class comandos{
     	n++;}}
     	leerArchivo.close();	
 		return n;}
-  Estudiante importar(){
+  void importar(Estudiante p[]){
     string linea;
     int n=0; //contar las lÃ­neas
     ifstream leerArchivo; //variable lÃ³gica de entrada
@@ -117,7 +43,7 @@ class comandos{
     n++;}}
     leerArchivo.close(); //cierre del flujo
     leerArchivo.open("database.txt");
-    Estudiante p[n];
+    
     for (int i=0; i<n; i++){
     getline(leerArchivo,linea,'|');
     p[i].codigo=atoi(linea.c_str());
@@ -133,9 +59,7 @@ class comandos{
     p[i].nota3=atoi(linea.c_str());
     getline(leerArchivo,linea);
     p[i].nota_final=atoi(linea.c_str());//falta un salto de linea
-
   }leerArchivo.close();
-  return *p;
   }
   void exportar(Estudiante p[], int n){
     ofstream escribirArchivo;
@@ -154,11 +78,11 @@ class Data_base: public comandos{
 	  };
 int main(){
   comandos cls;
-  Estudiante database[cls.num_db()]=cls.importar();
+  int tam = cls.num_db();
+  Estudiante *database = new Estudiante[tam];
+  cls.importar(database);
   for(int i=0;i<cls.num_db();++i){
   cout<<database[i].nombre<<endl;
   }
-  
   return 0;
 }
-*/
